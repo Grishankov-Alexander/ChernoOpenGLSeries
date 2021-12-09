ed57e186f9d515d5f2a72105ad07b285db86d406
Alexander Grishankov <grishankov.alexander@gmail.com>
2021-12-09 17:41:30 +0300

    OpenGL Day 2

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 
 
 
+#include <stdio.h>
 
 #include <gl/glew.h>
 #include <gl/glfw3.h>
@@ -18,14 +19,39 @@
 
 
 /*
-* 1 - Setting up
-* 
-* Uses Windows outdated API to OpenGL
-* to draw a triangle.
+* Use GLEW to load OpenGL functions from
+* GPU drivers.
+* Use GLFW to create a Window and drawing context.
 */
-int first(void)
+int createWindowWithGLEWAndGLFW(void);
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+     if (createWindowWithGLEWAndGLFW())
+        return -1;
+
+    return 0;
+}
+
+
+
+
+
+
+/*
+* Use GLEW to load OpenGL functions from
+* GPU drivers.
+* Use GLFW to create a Window and drawing context.
+*/
+int createWindowWithGLEWAndGLFW(void)
 {
     GLFWwindow* window;
+    GLenum err;
 
     /* Initialize the library */
     if (!glfwInit())
@@ -42,20 +68,64 @@ int first(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Initialize GLEW (Load OpenGL symbols from GPU drivers) */
+    glewExperimental = GL_TRUE;
+    err = glewInit();
+    if (err != GLEW_OK) {
+        /* GLEW init failed */
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        glfwTerminate();
+        return -1;
+    }
+    fprintf(stdout, "Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    fprintf(stdout, "Using OpenGL %s\n", glGetString(GL_VERSION));
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        /*
+        *   Rendering Begins Here
+        */
 
-        /* My Code */
+        /* Draw Triangle old way */
+        //{
+        //    const float points[] = {
+        //         0.0,  0.5, 0.0,
+        //         0.5, -0.5, 0.0,
+        //        -0.5, -0.5, 0.0
+        //    };
+        //    glBegin(GL_TRIANGLES);
+        //    glVertex2d(points[0], points[1]);
+        //    glVertex2d(points[3], points[4]);
+        //    glVertex2d(points[6], points[7]);
+        //    glEnd();
+        //}
+
+        /* Draw Triangle using modern OpenGL */
         {
-            glBegin(GL_TRIANGLES);
-            glVertex2d(-0.5, -0.5);
-            glVertex2d(0, 0.5);
-            glVertex2d(0.5, -0.5);
-            glEnd();
+                const float points[] = {
+                     0.0,  0.5, 0.0,
+                     0.5, -0.5, 0.0,
+                    -0.5, -0.5, 0.0
+                };
+                GLuint vbo, vao;
+
+                glGenBuffers(1, &vbo);
+                glBindBuffer(GL_ARRAY_BUFFER, vbo);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+                glGenVertexArrays(1, &vao);
+                glBindVertexArray(vao);
+                glEnableVertexAttribArray(0);
+                glBindBuffer(GL_ARRAY_BUFFER, vbo);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+
         }
+
+        /*
+        *   Rendering Ends Here
+        */
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -65,24 +135,4 @@ int first(void)
     }
 
     glfwTerminate();
-}
-
-
-/*
-* 2 - ...
-*/
-
-
-
-
-
-
-int main(int argc, char* argv[])
-{
-
-
-    if (first())
-        return -1;
-
-    return 0;
 }
